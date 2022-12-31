4eeacd85eece70b489739a53f36352b0c8415fc5
Vishal Sharma <vishals200415@gmail.com>
2022-12-31 12:36:36 +0530

    .

--- app.js
+++ app.js
@@ -27,13 +27,13 @@ const deadline = document.querySelector('.deadline');
 const items = document.querySelectorAll('.deadline-format h4');
 
 // code to always add 10 days when timer expires
-// let tempDate = new Date();
-// let tempYear = tempDate.getFullYear();
-// let tempMonth = tempDate.getMonth();
-// let tempDay = tempDate.getDay();
-// const futureDate = new Date(tempYear,tempMonth,tempDay+10,11,30,0);
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDay();
+const futureDate = new Date(tempYear,tempMonth,tempDay+10,11,30,0);
 
-let futureDate = new Date(2022,9,21,10,30,0); //comment this line to use above code
+// let futureDate = new Date(2022,9,21,10,30,0); //comment this line to use above code
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
